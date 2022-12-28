5a5a798f90bcd8576d9b4abb013ead091a7d35cf
EllyKwon <tok_o@naver.com>
2022-12-28 23:37:53 +0900

    네이버지도 크롤링 테스트 버전

--- webCrawler/webCrawler.py
+++ webCrawler/webCrawler.py
@@ -8,11 +8,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
+def naverMapCrawler() :
+    chromedriver = 'C:/chromedriver_win32/chromedriver'
+    driver = webdriver.Chrome(chromedriver)
 
-chromedriver = 'C:/chromedriver_win32/chromedriver'
-driver = webdriver.Chrome(chromedriver)
+    naver_map_search_url = f"https://m.map.naver.com/search2/search.naver?query=등촌동%20이디야&sm=hty&style=v5"
+    driver.get(naver_map_search_url)
 
-driver.get("https://sports.news.naver.com/kbaseball/index.nhn")
+    test = driver.find_element(By.CSS_SELECTOR, "#ct > div.search_listview._content._ctList > ul > li:nth-child(1) > div.item_info > a.a_item.a_item_distance._linkSiteview").get_attribute('data-cid')
+    print(test)
 
-title = driver.find_element(By.CSS_SELECTOR, "#content > div > div.home_feature > div.feature_side > div").text
-print(title)
+    # 상세 페이지
+    # https://m.place.naver.com/restaurant/
