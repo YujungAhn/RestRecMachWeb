37b49d9334624f4945593c26784530e758816b09
안유정 <dbwjd2575@gmail.com>
2024-05-31 22:00:23 +0900

    모델링 에러 처리 1. 결측치 처리 2. 데이터 삭제후 인덱스 맞춰주기

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -1,12 +1,14 @@
 import logging
+import typing
 
 from tqdm import tqdm_notebook
 
 import publicData.publicData as publicData
 from sklearn.feature_extraction.text import CountVectorizer  # 피체 벡터화
 from sklearn.metrics.pairwise import cosine_similarity  # 코사인 유사도
-import webCrawler
+import webCrawler.webCrawler as webCrawler
 import numpy as np
+import pandas as pd
 
 
 def recommandRes(name, areaCd, resCount):
@@ -17,10 +19,21 @@ def recommandRes(name, areaCd, resCount):
     resCount :
     """
     # 동별 상권정보 정보 조회
-    df = publicData.getAllStoreListInDong(areaCd)
+    df = pd.DataFrame(publicData.getAllStoreListInDong(areaCd)) # 메소드 리스트를 가지고 오지 못하는 에러로 dataframe으로 타입 초기화
 
-    # 음식점 데이터만 필터링
-    df = df.loc[df['상권업종대분류명'] == '음식']
+    rstRow = df[df['상호명'] == name]
+
+    # 음식점 데이터만 필터링 & 조회 속도를 높이기 위해 표준산업 분류명으로 추가 필터링
+    df = df[(df['상권업종대분류명'] == '음식') & (df['표준산업분류명'] == rstRow['표준산업분류명'].values[0])]
+
+    # 인덱스 재정렬
+    df = df.reset_index(drop=True)
+
+    #######  test   #####################################################
+    # # n개 행 만 두고 지우기
+    # df = df.truncate(after=20, axis=0)
+    # df = df.reset_index(drop=True)
+    ###########################################################
 
     # 사용하려는 컬럼만 필터링
     df = df[['상호명', '상권업종중분류명', '상권업종소분류명', '표준산업분류명', '행정동명', '위도', '경도']]
@@ -35,6 +48,65 @@ def recommandRes(name, areaCd, resCount):
                   'lat'  # 경도
                   ]
 
+    df['kakao_keyword'] = df['dong'] + "%20" + df['name']
+    df['kakao_map_url'] = ''
+
+    # 가게별 상세 페이지 url 찾기
+    webCrawler.initDriver()
+
+    for row in df.itertuples():
+        index = row.Index
+        keyword = row.name
+        print("이번에 찾을 키워드 :", keyword, f"/ {index} 행",)
+        print("Row 정보 :", row)
+        result = webCrawler.findUrlinKakaoMap(keyword)
+
+        try:
+            if pd.isna(result): # string null 체크
+                print("Nan Row 정보 :", row)
+                df.drop([index], axis=0, inplace=True)
+                print("FAil After")
+            else:
+                df.loc[index, 'kakao_map_url'] = result
+                print("Success After :", df.loc[index])
+        except Exception as e1:
+            print(e1)
+            df.drop([index], axis=0, inplace=True)
+            print("FAil Before :", )
+
+    # row 드롭 후 인덱스 재정렬
+    df = df.reset_index(drop=True)
+
+    # 상세 페이지 url에서 원하는 데이터를 추출
+    for i, url in enumerate(tqdm_notebook(df['kakao_map_url'])):
+        try:
+            print(f"{i}행 데이터 : {url}")
+            reviewData = webCrawler.findReviewInKakaoMapUrl(url)
+
+            if pd.isna(reviewData.star_review_qty) or reviewData.star_review_qty == "":  # 별점 평가가 안되는 잘못 조회된 데이터에 대한 처리
+                df.drop([i], axis=0, inplace=True)
+                continue
+
+            df.loc[i, 'kakao_store_name'] = reviewData.kakao_map_name  # 카카오 상세페이지에서 크롤링한 상호명
+            df.loc[i, 'kakao_star_point'] = reviewData.star_review_stars  # 카카오 상세페이지에서 평가한 별점 평점
+            df.loc[i, 'kakao_star_point_qty'] = reviewData.star_review_qty  # 카카오 상세페이지에서 별점 평가를 한 횟수
+            df.loc[i, 'kakao_blog_review_txt'] = reviewData.review_text  # 카카오 상세페이지에 나온 블로그 리뷰 텍스트들
+            df.loc[i, 'kakao_blog_review_qty'] = reviewData.blog_review_qty  # 카카오 상세페이지에 나온 블로그 리뷰의 총 개수
+
+        except Exception as e1:
+            print(e1)
+
+    webCrawler.quitDriver()
+
+    # row 드롭 후 인덱스 재정렬
+    df = df.reset_index(drop=True)
+
+    # 공백 NaN 데이터 전처리
+    mean_value = df['kakao_star_point'].mean()
+    df['kakao_star_point'].fillna(mean_value, inplace=True)
+    df['kakao_star_point_qty'].fillna(0, inplace=True)
+    df['kakao_blog_review_qty'].fillna(0, inplace=True)
+
     # 카테고리 데이터들을 하나로 묶는 전처리
     df['cate_mix'] = df['cate_1'] + df['cate_2'] + df['cate_3']
 
@@ -48,42 +120,9 @@ def recommandRes(name, areaCd, resCount):
     # 코사인 유사도 계산
     place_simi_cate = cosine_similarity(place_category, place_category)
 
-    df['kakao_keyword'] = df['dong'] + "%20" + df['name']
-    df['kakao_map_url'] = ''
-
-    # 각 데이터들을 미리 리스트에 담은 다음, 마지막에 데이터 프레임에 합칠 것입니다.
-
-    kakao_map_name_list = []
-    blog_review_list = []
-    blog_review_qty_list = []
-    kakao_map_star_review_stars_list = []
-    kakao_map_star_review_qty_list = []
-
-    # 가게별 상세 페이지 url 찾기
-    for i, keyword in enumerate(df['kakao_keyword'].tolist()):
-        print("이번에 찾을 키워드 :", i, f"/ {df.shape[0]} 행", keyword)
-        df.iloc[i, -1] = webCrawler.findUrlinKakaoMap(keyword)
-
-    # 상세 페이지 url에서 원하는 데이터를 추출
-    for i, url in enumerate(tqdm_notebook(df['kakao_map_url'])):
-        print(f"{i}행 데이터 : {url}")
-        reviewData = webCrawler.findReviewInKakaoMapUrl(url)
-
-        blog_review_list.append(reviewData.review_text)
-        kakao_map_name_list.append(reviewData.kakao_map_name)
-        blog_review_qty_list.append(reviewData.blog_review_qty)
-        kakao_map_star_review_stars_list.append(reviewData.star_review_stars)
-        kakao_map_star_review_qty_list.append(reviewData.star_review_qty)
-
-    df['kakao_store_name'] = kakao_map_name_list  # 카카오 상세페이지에서 크롤링한 상호명
-    df['kakao_star_point'] = kakao_map_star_review_stars_list  # 카카오 상세페이지에서 평가한 별점 평점
-    df['kakao_star_point_qty'] = kakao_map_star_review_qty_list  # 카카오 상세페이지에서 별점 평가를 한 횟수
-    df['kakao_blog_review_txt'] = blog_review_list  # 카카오 상세페이지에 나온 블로그 리뷰 텍스트들
-    df['kakao_blog_review_qty'] = blog_review_qty_list  # 카카오 상세페이지에 나온 블로그 리뷰의 총 개수
-
     # 리뷰 텍스트 데이터 간의 텍스트 피쳐 벡터라이징
     count_vect_review = CountVectorizer(min_df=2, ngram_range=(1, 2))
-    place_review = count_vect_review.fit_transform(df['kakao_blog_review_txt'])
+    place_review = count_vect_review.fit_transform(df['kakao_blog_review_txt'].astype(str))
 
     # 리뷰 텍스트 간의 코사인 유사도 따지기
     place_simi_review = cosine_similarity(place_review, place_review)
@@ -111,6 +150,6 @@ def recommandRes(name, areaCd, resCount):
 
     ## 종합 유사도 평사 순위 리스트에서 검색한 상가와 유사도가 높은 상가를 찾는다
     place_index = df[df['name'] == name].index.values
-    similar_indexes = place_simi_co_sorted_ind[place_index, :resCount]
+    similar_indexes = place_simi_co_sorted_ind[place_index, :resCount+1] # 출력을 원하는 유사한 음식점의 개수
     similar_indexes = similar_indexes.reshape(-1)
     return df.iloc[similar_indexes]
