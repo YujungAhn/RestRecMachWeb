16600adb2eb35d86e89a85c389dce10cb01c705b
안유정 <dbwjd2575@gmail.com>
2024-05-31 21:53:42 +0900

    selenium element 조회 함수 최신 지원버전으로 수정

--- webCrawler/webCrawler.py
+++ webCrawler/webCrawler.py
@@ -1,31 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 import time
 import numpy as np
 import tqdm
-
+from modeling.reviewData import ReviewData
 from modeling import reviewData
 
-chromedriver = 'C:/chromedriver_win32/chromedriver'
+# chromedriver = 'C:\chromedriver_win32\chromedriver\chrome-win64\chrome.exe' # 해당 경로에 버전에 맞는 크롬드라이버 다운로드 https://chromedriver.chromium.org/downloads
 driver = None
-
+# service = Service("C:\Windows\chromedriver.exe") driver = webdriver.Chrome(service=service)
 
 def findUrlinKakaoMap(keyword):
     '''
     kakao Map에서 keyword로 검색하여 상세 페이지 주소 찾기
     '''
+    result = ''
     kakao_map_search_url = f"https://map.kakao.com/?q={keyword}"
     try:
         driver.get(kakao_map_search_url)
         time.sleep(3.5)
         # 먼저 시도
-        result = driver.find_element_by_css_selector(
+        result = driver.find_element(By.CSS_SELECTOR,
             "#info\.search\.place\.list > li:nth-child(1) > div.info_item > div.contact.clickArea > a.moreview").get_attribute(
             'href')
 
@@ -33,7 +29,7 @@ def findUrlinKakaoMap(keyword):
     except Exception as e1:
         if "li:nth-child(1)" in str(e1):  # child(1)이 없던데요?
             try:
-                result = driver.find_element_by_css_selector(
+                result = driver.find_element(By.CSS_SELECTOR,
                     "#info\.search\.place\.list > li > div.info_item > div.contact.clickArea > a.moreview").get_attribute(
                     'href')
                 time.sleep(1)
@@ -61,22 +57,22 @@ def findReviewInKakaoMapUrl(url) -> reviewData:
     driver.get(url)
     time.sleep(1)
     review_text = ""
-    result = {}
+    reviewData = ReviewData("","","","","")
 
     try:
-        kakao_map_name = driver.find_element_by_css_selector(
+        kakao_map_name = driver.find_element(By.CSS_SELECTOR,
             "#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > h2").text
-        blog_review_qty = driver.find_element_by_css_selector(
+        blog_review_qty = driver.find_element(By.CSS_SELECTOR,
             "#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > a:nth-child(5) > span").text
-        star_review_stars = driver.find_element_by_css_selector(
+        star_review_stars = driver.find_element(By.CSS_SELECTOR,
             "#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > a:nth-child(3) > span.color_b.inactive").text
-        star_review_qty = driver.find_element_by_css_selector(
+        star_review_qty = driver.find_element(By.CSS_SELECTOR,
             "#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > a:nth-child(3) > span.color_g").text
     except Exception as e1:
         if "inactive" in str(e1):
-            star_review_stars = driver.find_element_by_css_selector(
+            star_review_stars = driver.find_element(By.CSS_SELECTOR,
                 "#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > a:nth-child(3) > span.color_b").text
-            star_review_qty = driver.find_element_by_css_selector(
+            star_review_qty = driver.find_element(By.CSS_SELECTOR,
                 "#mArticle > div.cont_essential > div:nth-child(1) > div.place_details > div > div > a:nth-child(3) > span.color_g").text
         else:
             print(f"\'{url}\' 데이터의 평점 갯수 크롤링에 다른 문제가 생겼다")
@@ -86,15 +82,15 @@ def findReviewInKakaoMapUrl(url) -> reviewData:
         while num < 3:
             try:
                 num += 1
-                review_text = review_text + driver.find_element_by_css_selector(
+                review_text = review_text + driver.find_element(By.CSS_SELECTOR,
                     f"div.wrap_list > ul > li:nth-child({num}) > a > div.review_story > p").text
 
                 if num == 3:
                     reviewData.review_text = review_text  # 3개나 찾았으면 그만 찾고 나감
                     reviewData.kakao_map_name = kakao_map_name
-                    reviewData.blog_review_qty = blog_review_qty
-                    reviewData.star_review_stars = star_review_stars
-                    reviewData.star_review_qty = star_review_qty
+                    reviewData.blog_review_qty = int(blog_review_qty)
+                    reviewData.star_review_stars = float(star_review_stars)
+                    reviewData.star_review_qty = int(star_review_qty.strip("("")"))
                     break
             except Exception as e1:
                 if "li:nth-child(1)" in str(e1):  # child(1)이 없던데요? -> 리뷰가 하나도 없는 것
@@ -102,28 +98,32 @@ def findReviewInKakaoMapUrl(url) -> reviewData:
                     review_text = "empty"
                     reviewData.review_text = review_text
                     reviewData.kakao_map_name = kakao_map_name
-                    reviewData.blog_review_qty = blog_review_qty
-                    reviewData.star_review_stars = star_review_stars
-                    reviewData.star_review_qty = star_review_qty
+                    reviewData.blog_review_qty = int(blog_review_qty)
+                    reviewData.star_review_stars = float(star_review_stars)
+                    reviewData.star_review_qty = int(star_review_qty.strip("("")"))
                     break
                 else:
                     print(f"문제가 발생 - 리뷰가 {num - 1}개뿐이다")
                     reviewData.review_text = review_text  # 일단 리뷰가 하나도 없는 건 아니니 붙이고 탈출 / 리뷰 딸랑 하나 있으면 발생할 수 있음
                     reviewData.kakao_map_name = kakao_map_name
-                    reviewData.blog_review_qty = blog_review_qty
-                    reviewData.star_review_stars = star_review_stars
-                    reviewData.star_review_qty = star_review_qty
+                    reviewData.blog_review_qty = int(blog_review_qty)
+                    reviewData.star_review_stars = float(star_review_stars)
+                    reviewData.star_review_qty = int(star_review_qty.strip("("")"))
                     break
 
     except Exception as e2:
         print(e2)
         print(f"문제 발생 - code 2")
-    return result
+    return reviewData
 
 
 def initDriver():
     global driver
-    driver = webdriver.Chrome(chromedriver)
+    service = Service()
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    # Chrome 116버전 업데이트 -> Selenium 버전 업데이트 : 자동 드라이버 탐색 기능 추가
+    # https://stackoverflow.com/questions/76913935/selenium-common-exceptions-sessionnotcreatedexception-this-version-of-chromedri
 
 
 def quitDriver():
